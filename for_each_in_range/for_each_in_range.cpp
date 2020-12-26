28be6f0f9b085172cdeb34d0eb99da8bbfd9a714
adam-lafontaine <adam.lafontaine@gmail.com>
2020-12-26 06:19:54 -0500

    add files

---
+++ for_each_in_range/for_each_in_range.cpp
@@ -0,0 +1,21 @@
+#include "index_range.hpp"
+#include "point_range_2d.hpp"
+
+#include <functional>
+#include <algorithm>
+#include <execution>
+
+using id_func_t = std::function<void(size_t id)>;
+
+void for_each_in_range(size_t size, id_func_t const& id_func)
+{
+    IndexRange ids(size);
+	std::for_each(ids.begin(), ids.end(), id_func);
+}
+
+
+void for_each_in_range_par(size_t size, id_func_t const& id_func)
+{
+    IndexRange ids(size);
+	std::for_each(std::execution::par, ids.begin(), ids.end(), id_func);
+}
