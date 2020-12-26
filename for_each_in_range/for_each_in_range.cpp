9e0d9512966087419a558dfcd27497bd1975f5e7
adam-lafontaine <adam.lafontaine@gmail.com>
2020-12-26 07:35:59 -0500

    range classes

--- for_each_in_range/for_each_in_range.cpp
+++ for_each_in_range/for_each_in_range.cpp
@@ -9,13 +9,27 @@ using id_func_t = std::function<void(size_t id)>;
 
 void for_each_in_range(size_t size, id_func_t const& id_func)
 {
-    IndexRange ids(size);
+    UnsignedRange ids(size);
+	std::for_each(ids.begin(), ids.end(), id_func);
+}
+
+
+void for_each_in_range(size_t begin, size_t end, id_func_t const& id_func)
+{
+    UnsignedRange ids(begin, end);
 	std::for_each(ids.begin(), ids.end(), id_func);
 }
 
 
 void for_each_in_range_par(size_t size, id_func_t const& id_func)
 {
-    IndexRange ids(size);
+    UnsignedRange ids(size);
+	std::for_each(std::execution::par, ids.begin(), ids.end(), id_func);
+}
+
+
+void for_each_in_range_par(size_t begin, size_t end, id_func_t const& id_func)
+{
+    UnsignedRange ids(begin, end);
 	std::for_each(std::execution::par, ids.begin(), ids.end(), id_func);
 }
